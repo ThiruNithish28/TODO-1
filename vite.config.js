431f8f20e915fdfe5352b49c5a36c7128077e20d
ThiruNithish28 <valavannithish.007@gmail.com>
2024-12-08 22:03:52 +0530

    fix the hosting error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/TODO-1/'
+   base:'/TODO-1/',
+  plugins: [react()]
+ 
 })
